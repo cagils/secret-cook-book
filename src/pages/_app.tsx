9356a6c57fc3a1f989a69b01bface6fd6e5795cb
Cagil Seker <cagils@gmail.com>
2022-04-19 12:10:02 +0300

    Added mantine integration

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,14 +1,36 @@
 import { AppProps } from 'next/app';
+import Head from 'next/head';
+import { MantineProvider } from '@mantine/core';
 
 import Layout from '../components/Layout/Layout';
 import '../styles/globals.css';
 
-function MyApp({ Component, pageProps }: AppProps) {
+function App(props: AppProps) {
+  const { Component, pageProps } = props;
   return (
-    <Layout>
-      <Component {...pageProps} />
-    </Layout>
+    <>
+      <Head>
+        <title>Page title</title>
+        <meta
+          name="viewport"
+          content="minimum-scale=1, initial-scale=1, width=device-width"
+        />
+      </Head>
+
+      <MantineProvider
+        withGlobalStyles
+        withNormalizeCSS
+        theme={{
+          /** Put your mantine theme override here */
+          colorScheme: 'dark',
+        }}
+      >
+        <Layout>
+          <Component {...pageProps} />
+        </Layout>
+      </MantineProvider>
+    </>
   );
 }
 
-export default MyApp;
+export default App;
