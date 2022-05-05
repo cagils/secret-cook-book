37ed6a7f537cc127bb633c6f995499bfcf548c5e
Cagil Seker <cagils@gmail.com>
2022-05-05 13:25:37 +0300

    feat: style improvements for ingredients component

--- src/components/create-recipe/Ingredients/Ingredients.jsx
+++ src/components/create-recipe/Ingredients/Ingredients.jsx
@@ -1,37 +1,81 @@
 import {
+  Box,
   Button,
   ButtonGroup,
+  Center,
+  Circle,
+  Container,
+  Flex,
+  HStack,
   Icon,
   IconButton,
   Input,
   Select,
+  Spacer,
+  Square,
+  Stack,
   useColorMode,
+  VStack,
 } from '@chakra-ui/react';
 import { MinusSquare, Moon, PlusSquare, Sun } from '@emotion-icons/feather';
 import Link from 'next/link';
 import { useState } from 'react';
 
-const Ingredient = ({ plus = false, name, qty, unit }) => {
+const Ingredient = ({ plus = false, name, qty, unit = 'gr' }) => {
   return (
-    <>
-      <Input placeholder="Item name" value={name} onChange={() => null} />
-
-      <Input placeholder="qty" value={qty} onChange={() => null} />
-
-      <Select placeholder="unit" value={unit} onChange={() => null}>
-        <option value="gr">gr.</option>
-        <option value="tbsp">tbsp.</option>
-        <option value="tsp">tsp.</option>
-        <option value="pinch">pinch</option>
-      </Select>
-
-      <IconButton
-        size="lg"
-        aria-label="Toggle Dark Mode"
-        fontSzie="20px"
-        icon={<Icon as={plus ? PlusSquare : MinusSquare} />}
-      />
-    </>
+    <Box>
+      <Flex grow="1" my={{ base: '4px', md: '6px' }}>
+        <Flex grow="1" gap={{ base: '4px', md: '6px' }} wrap="wrap">
+          <Input
+            minWidth={{ base: '100px', md: '300px' }}
+            flexBasis="300px"
+            flexGrow="2"
+            isRequired
+            variant="filled"
+            placeholder="Item name"
+            value={name}
+            onChange={() => null}
+          />
+          <Flex gap={{ base: '4px', md: '6px' }}>
+            <Input
+              minWidth={{ base: '100px', md: '150px' }}
+              flexBasis="150px"
+              flexGrow="1"
+              isRequired
+              variant="filled"
+              placeholder="qty"
+              value={qty}
+              onChange={() => null}
+              textAlign="end"
+            />
+            <Select
+              minWidth={{ base: '100px', md: '150px' }}
+              flexBasis="150px"
+              flexGrow="1"
+              isRequired
+              variant="filled"
+              value={unit}
+              onChange={() => null}
+            >
+              <option value="gr">gr.</option>
+              <option value="tbsp">tbsp.</option>
+              <option value="tsp">tsp.</option>
+              <option value="pinch">pinch</option>
+            </Select>
+          </Flex>
+        </Flex>
+        <Square>
+          <IconButton
+            flexBasis="1"
+            isRound
+            aria-label="Toggle Dark Mode"
+            fontSize="1.2rem"
+            variant="ghost"
+            icon={<Icon as={plus ? PlusSquare : MinusSquare} />}
+          />
+        </Square>
+      </Flex>
+    </Box>
   );
 };
 
@@ -40,19 +84,28 @@ const Ingredients = () => {
   const dark = colorMode === 'dark';
 
   return (
-    <div>
-      <div>Ingredients</div>
-      <Ingredient name="Beef" qty={250} unit="gr" />
-      <Ingredient name="Rice" qty={150} unit="gr" />
-      <Ingredient name="Salt" qty={2} unit="pinch" />
-      <Ingredient plus />
-      <Button
-        onClick={() => toggleColorMode()}
-        variant="gradient"
-        bgGradient="linear(to-r, green.200, pink.500)"
-      >
-        Save Recipe
-      </Button>
+    <Box>
+      <Box>Ingredients</Box>
+      <Box m="8px" justify="center" align="center" grow="1">
+        <Box maxWidth="1200px" justify="center" align="center">
+          <Box>
+            <Ingredient name="Beef" qty={250} unit="gr" />
+            <Ingredient name="Rice" qty={150} unit="gr" />
+            <Ingredient name="Salt" qty={2} unit="pinch" />
+            <Ingredient plus />
+          </Box>
+          <Box my="8px" align="end">
+            <Button
+              color="white"
+              onClick={() => null}
+              variant="gradient"
+              bgGradient="linear(to-r, purple.300, pink.300)"
+            >
+              Save Recipe
+            </Button>
+          </Box>
+        </Box>
+      </Box>
       <Button
         onClick={() => toggleColorMode()}
         variant="gradient"
@@ -67,7 +120,7 @@ const Ingredients = () => {
         aria-label="Toggle color scheme"
         icon={<Icon as={dark ? Sun : Moon} />}
       />
-    </div>
+    </Box>
   );
 };
 
