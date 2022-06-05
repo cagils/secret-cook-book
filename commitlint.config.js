551acb62fe4173d75dbf1a15294b2ad66e8dea20
Cagil Seker <cagils@gmail.com>
2022-06-05 12:28:01 +0300

    build: test commitlint

--- commitlint.config.js
+++ commitlint.config.js
@@ -87,36 +87,6 @@ module.exports = {
           },
         },
       },
-      scope: {
-        description:
-          'What is the scope of this change (e.g. component or file name)',
-      },
-      subject: {
-        description: 'Write a short description of the change',
-      },
-      body: {
-        description: 'Provide a longer description of the change',
-      },
-      isBreaking: {
-        description: 'Are there any breaking changes?',
-      },
-      breakingBody: {
-        description:
-          'A BREAKING CHANGE commit requires a body. Please enter a longer description of the commit itself',
-      },
-      breaking: {
-        description: 'Describe the breaking changes',
-      },
-      isIssueAffected: {
-        description: 'Does this change affect any open issues?',
-      },
-      issuesBody: {
-        description:
-          'If issues are closed, the commit requires a body. Please enter a longer description of the commit itself',
-      },
-      issues: {
-        description: 'Add issue references (e.g. "fix #123", "re #123".)',
-      },
     },
   },
 };
