1fed1a3b6da879b6bf679af4a025003e6c762b48
Cagil Seker <cagils@gmail.com>
2022-07-28 13:33:23 +0300

    removed unused test files

--- __tests__/index.test.jsx
+++ __tests__/index.test.jsx
@@ -1,4 +1,4 @@
-import '@testing-library/jest-dom';
+/* import '@testing-library/jest-dom';
 import { render, screen } from '@testing-library/react';
 import Home from '../src/pages/index';
 
@@ -12,4 +12,4 @@ describe('Home', () => {
 
     expect(link).toBeInTheDocument();
   });
-});
+}); */
