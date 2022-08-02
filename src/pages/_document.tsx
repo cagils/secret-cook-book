1f850844bc7de957662c4591ca6c6446b3bf25c1
Cagil Seker <cagils@gmail.com>
2022-08-02 17:28:26 +0300

    prevent FOUT (Flash of unwanted typeface) by Next JS automatic preload

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -7,7 +7,49 @@ export default function Document() {
   return (
     <Html lang="en">
       {/* TODO support other languages */}
-      <Head>{/* Third party links, scripts */}</Head>
+      <Head>
+        {/* Third party links, scripts */}
+
+        {/* // GOOGLE WEB FONTS */}
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+        <link
+          rel="stylesheet"
+          href="https://fonts.googleapis.com/css2?family=Roboto:wght@100&display=block"
+        />
+        <link
+          rel="stylesheet"
+          href="https://fonts.googleapis.com/css2?family=Nunito:ital,wght@0,200;0,300;0,400;0,500;1,200;1,300;1,400;1,500&display=block"
+        />
+        <link
+          rel="stylesheet"
+          href="https://fonts.googleapis.com/css2?family=Quicksand:wght@300;400;500;600&display=block"
+        />
+        <link
+          rel="stylesheet"
+          href="https://fonts.googleapis.com/css2?family=Big+Shoulders+Display:wght@100;200;300;400;500;600;700;800&display=block"
+        />
+        <link
+          rel="stylesheet"
+          href="https://fonts.googleapis.com/css2?family=Baloo+Thambi+2:wght@400;500;600&display=block"
+        />
+        <link
+          rel="stylesheet"
+          href="https://fonts.googleapis.com/css2?family=Sansita+Swashed:wght@300;400;500;600;700;800&display=block"
+        />
+        <link
+          rel="stylesheet"
+          href="https://fonts.googleapis.com/css2?family=PT+Sans+Caption:wght@400;700&display=block"
+        />
+        <link
+          rel="stylesheet"
+          href="https://fonts.googleapis.com/css2?family=Caveat:wght@400;500;600;700&display=block"
+        />
+        <link
+          rel="stylesheet"
+          href="https://fonts.googleapis.com/css2?family=Caveat+Brush&display=block"
+        />
+      </Head>
       <body>
         <ColorModeScript
           initialColorMode={customTheme.config.initialColorMode}
