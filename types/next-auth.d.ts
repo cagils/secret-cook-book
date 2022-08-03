34cc7bb48114ad8e4b2c6542972aa9ed1fdb69c4
Cagil Seker <cagils@gmail.com>
2022-08-03 16:35:51 +0300

    next auth user role and typescript

---
+++ types/next-auth.d.ts
@@ -0,0 +1,31 @@
+import { Role } from '@prisma/client';
+import NextAuth, { DefaultSession } from 'next-auth';
+import { JWT } from 'next-auth/jwt';
+
+declare module 'next-auth/jwt' {
+  /** Returned by the `jwt` callback and `getToken`, when using JWT sessions */
+  interface JWT {
+    id?: string;
+    provider?: string;
+    role?: Role;
+  }
+}
+
+declare module 'next-auth' {
+  /**
+   * Returned by `useSession`, `getSession` and received as a prop on the
+   * `SessionProvider` React Context and trpc context
+   */
+  interface Session {
+    user?: {
+      id?: string;
+      provider?: string;
+      role?: Role;
+    } & DefaultSession['user'];
+  }
+
+  /** Passed as a parameter to the `jwt` callback */
+  interface User {
+    role?: Role;
+  }
+}
